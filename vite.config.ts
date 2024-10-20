4fc9fbf08d173a045cceef340c783a5ea7214cfe
Anynulll <koga2249@gmail.com>
2024-10-20 22:29:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://newsapi.org/v2',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          configure: (proxy, _options) => {
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              proxyReq.setHeader('Authorization', `Bearer ${env.VITE_NEWS_API_KEY}`);
-            });
-          },
-        },
+server: {
+  host: true, // 外部ネットワークでのアクセスを許可
+  port: process.env.PORT || 5173, // 環境変数PORTまたはデフォルトでポート5173を使用
+  proxy: {
+    '/api': {
+      target: 'https://newsapi.org/v2',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
+      configure: (proxy, _options) => {
+        proxy.on('proxyReq', (proxyReq, req, _res) => {
+          proxyReq.setHeader('Authorization', `Bearer ${env.VITE_NEWS_API_KEY}`);
+        });
       },
     },
-  };
-});
+  },
+}
